c0532a317d523eaaef005f4e96d2bd655756c831
solareenlo <solareenlo@gmail.com>
2018-10-27 02:15:14 +0900

    JSXは可読性よく書ける

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello World!</h1></div>;
+    return <h1>Hello World!</h1>;
   }
 }
+
+// こんな風にも書けるが上の方が直感的でわかりやすい
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello World!"
+//     );
+//   }
+// }
 
 export default App;
