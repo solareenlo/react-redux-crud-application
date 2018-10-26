6c939d83df5f1668ad6a5444a0873d41fa7f81e1
solareenlo <solareenlo@gmail.com>
2018-10-27 03:42:40 +0900

    propsで複数個の変数を渡す

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <User name={"Taro"} />
+      <User name={"Taro"} age={10} />
     </div>
   )
 }
 
 const User = (props) => {
-  return <div>Hi, I am {props.name}! </div>
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
 
 export default App;
