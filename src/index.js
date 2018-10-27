4c795fabf24ef741bc8c3e4b5fe4ceaf687afed1
solareenlo <solareenlo@gmail.com>
2018-10-28 03:39:16 +0900

    New Eventで飛んでった先にfooを描写

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
@@ -16,9 +17,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        {/*
         <Route exact path="/events/new" component={EventsNew}/>
-        */}
         <Route exact path="/" component={EventsIndex}/>
       </Switch>
     </BrowserRouter>
