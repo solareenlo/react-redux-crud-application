a35e21216acbf0edb44ed09c51c4cb8f8a15bc67
solareenlo <solareenlo@gmail.com>
2018-10-28 03:37:15 +0900

    fixed typo

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -35,7 +35,7 @@ class EventsIndex extends Component {
             {this.renderEvents()}
           </tbody>
         </table>
-        <Link to="/event/new">New Event</Link>
+        <Link to="/events/new">New Event</Link>
       </React.Fragment>
     )
   }
