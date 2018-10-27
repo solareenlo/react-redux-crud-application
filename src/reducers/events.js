f7085cb769cc81448b0a289a7663739f02e5c41c
solareenlo <solareenlo@gmail.com>
2018-10-28 02:12:47 +0900

    再配列した配列をリターン

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,16 +1,11 @@
 import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
-export default (state = {}, action) => {
+export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
-      // ↓こっちは配列
-      console.log(action.response.data)
-      // ↓こっちはオブジェクト lodashを使って上の配列を再配列した
-      // idをキーにして
-      console.log(_.mapKeys(action.response.data, 'id'))
-      return state
+      return _.mapKeys(action.response.data, 'id')
     default:
-      return state
+      return events
   }
 }
