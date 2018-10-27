7e1d565edc39d8c1efe31c95aca64f24586b2b2a
solareenlo <solareenlo@gmail.com>
2018-10-28 02:07:53 +0900

    rodashを使って配列を、idをkeyにして、扱いやすいように、再配列した

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,8 +1,14 @@
+import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
 export default (state = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
+      // ↓こっちは配列
+      console.log(action.response.data)
+      // ↓こっちはオブジェクト lodashを使って上の配列を再配列した
+      // idをキーにして
+      console.log(_.mapKeys(action.response.data, 'id'))
       return state
     default:
       return state
