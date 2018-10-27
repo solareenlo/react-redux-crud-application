cc2a1efa61aac794010a487d9080567ad44352fc
solareenlo <solareenlo@gmail.com>
2018-10-27 15:47:57 +0900

    storeの実装

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import reducer from './reducers';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={sotre}>
+    <App />
+  </Provider>
+  document.getElementById('root')
+);
+registerServiceWorker();
